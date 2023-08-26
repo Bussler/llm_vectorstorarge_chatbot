5a579dccd0c17f999184ffa5edb461d66101adc8
Bussler <maarten.bussler@gmail.com>
2023-08-26 15:53:27 +0200

    Refactor Modelsetup into own file

--- backend/main.py
+++ backend/main.py
@@ -6,18 +6,17 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-from langchain.llms import HuggingFacePipeline
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 
-import transformers
-import torch
 import huggingface_hub
 
 import uvicorn
+
+from setup_model import setup_llm
 
 
 app = FastAPI()
@@ -31,60 +30,12 @@ chat_history = [[]]
 class promt_request(BaseModel):
     q: str
     use_chat: int = 0
-
-def setup_llm():
-    model_id="bigscience/bloom-560m"
-    device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
-
-    model_config = transformers.AutoConfig.from_pretrained(
-        model_id,
-    )
-
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_id,
-        config=model_config,
-        device_map=device,
-    )
     
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id,)
-
-    # enable evaluation mode to allow model inference
-    model.eval()
-    print(f"Model loaded on {device}")
-
-    stop_list = ['\nHuman:', '\n```\n']
-    stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
-    stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
-
-    # define custom stopping criteria object
-    class StopOnTokens(transformers.StoppingCriteria):
-        def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-            for stop_ids in stop_token_ids:
-                if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
-                    return True
-            return False
-
-    stopping_criteria = transformers.StoppingCriteriaList([StopOnTokens()])
-
-    generate_text = transformers.pipeline(
-        model=model, 
-        tokenizer=tokenizer,
-        return_full_text=True,
-        task='text-generation',
-        # we pass model parameters here too
-        stopping_criteria=stopping_criteria,
-        temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
-        max_new_tokens=64,  # max number of tokens to generate in the output
-    )
-
-    llm = HuggingFacePipeline(pipeline=generate_text)
-    return llm
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
-    hugging_face_token = ''
+    hugging_face_token = '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'
     huggingface_hub.login(token=hugging_face_token)
     
     print("Set up llm")
---
+++ backend/setup_model.py
@@ -0,0 +1,52 @@
+import torch
+import transformers
+from langchain.llms import HuggingFacePipeline
+
+
+def setup_llm(model_id="bigscience/bloom-560m"):
+    model_id="bigscience/bloom-560m"
+    device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
+
+    model_config = transformers.AutoConfig.from_pretrained(
+        model_id,
+    )
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        config=model_config,
+        device_map=device,
+    )
+    
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id,)
+
+    # enable evaluation mode to allow model inference
+    model.eval()
+    print(f"Model loaded on {device}")
+
+    stop_list = ['\nHuman:', '\n```\n']
+    stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
+    stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
+
+    # define custom stopping criteria object
+    class StopOnTokens(transformers.StoppingCriteria):
+        def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+            for stop_ids in stop_token_ids:
+                if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
+                    return True
+            return False
+
+    stopping_criteria = transformers.StoppingCriteriaList([StopOnTokens()])
+
+    generate_text = transformers.pipeline(
+        model=model, 
+        tokenizer=tokenizer,
+        return_full_text=True,
+        task='text-generation',
+        # we pass model parameters here too
+        stopping_criteria=stopping_criteria,
+        temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+        max_new_tokens=64,  # max number of tokens to generate in the output
+    )
+
+    llm = HuggingFacePipeline(pipeline=generate_text)
+    return llm
