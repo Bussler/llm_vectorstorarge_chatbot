b96450d4834eb2af6614de2f1c0d833405fe58d1
Bussler <maarten.bussler@gmail.com>
2023-08-29 21:16:25 +0200

    Add own logo

--- frontend/min-example/src/App.vue
+++ frontend/min-example/src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Bug Gang logo" class="logo" src="@/assets/marioBugGang.png" width="170" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Welcome!" />
--- frontend/min-example/src/views/AboutView.vue
+++ frontend/min-example/src/views/AboutView.vue
@@ -12,8 +12,8 @@ export default{
     <h1>Hi!</h1>
 
     <div>
-      <p>Use this app to communicate with the chatbot mounted in /backend.</p>
-      <p>You can add documents to the /backend/text_data/ folder to query specific information about them.</p>
+      <p>Use this app to communicate with the chatbot mounted in /backend and chat on your own files.</p>
+      <p>You can add documents to the /backend/text_data/ folder or upload them directly via the "Add File" button.</p>
     </div>
   </main>
 
